19e628ab09adf06382b58cb8a410338ebb2bcdaf
my_git <primespeed3@gmail.com>
2018-06-27 17:50:49 +0100

    setup startscripa nd heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
+
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
@@ -52,4 +54,4 @@ app.get('/bad', (req, res) => {
     });
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
